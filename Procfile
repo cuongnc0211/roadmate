ee8b4e11cb40f33403348dd86ad5ae5422a09295
cuongnc0211 <cuongnguyenfu@gmail.com>
2026-03-16 16:40:37 +0700

    config railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+rake db:prepare && bin/rails server -b 0.0.0.0 -p ${PORT:-3000}
--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module Roadmate
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.hosts << "roadmate-staging.up.railway.app"
+    config.hosts << "roadmate.com"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
